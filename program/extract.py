bb6e2b451ebda117c0eaf4297f58e2dad9831290
Eric Dasse <christ.gououi@gmail.com>
2022-01-31 14:27:13 +0100

    [Feat] Method to retrieve songs listened to prior to a specific time within a given time range from the Spotify API

---
+++ program/extract.py
@@ -0,0 +1,62 @@
+import datetime
+import pandas as pd
+import requests
+from dateutil.parser import parse
+
+
+class Extract:
+
+    TOKEN = ""
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {TOKEN}"
+    }
+    today = datetime.datetime.now()
+
+    def retrieve_songs(self, reference_date="now", days=30) -> pd.DataFrame:
+        """Retrieve songs that were listened on Spotify prior to a specific time"""
+        if reference_date == "now":
+            reference_date_object = self.today
+        else:
+            reference_date_split = reference_date.split("/")
+            reference_date_object = datetime.datetime(reference_date_split[2],
+                                                      reference_date_split[1],
+                                                      reference_date_split[0])
+
+        past_date = reference_date_object - datetime.timedelta(days=1)
+        past_date_timestamp_ms = int(past_date.timestamp()) * 1000
+
+        # Request to Spotify
+        r = requests\
+            .get("https://api.spotify.com/v1/me/player/recently-played?after={time}")\
+            .format(time=past_date_timestamp_ms, headers=self.headers)
+
+        data = r.json()
+
+        song_names = []
+        artist_names = []
+        played_at_list = []
+        timestamps = []
+
+        for song in data["items"]:
+            song_names.append(song["track"]["name"])
+            artist_names.append(song["track"]["album"]["artists"][0]["name"])
+            played_at_list.append(song["played_at"])
+            timestamps.append(song["played_at"][:10])
+
+        song_dict = {
+            "song_name": song_names,
+            "artist_name": artist_names,
+            "played_at": played_at_list,
+            "timestamp": timestamps
+        }
+
+        return pd.DataFrame(song_dict)
+
+    def retrieve_yesterday_songs(self):
+        yesterday = self.today - datetime.timedelta(days=1)
+        yesterday_unix_timestamp_ms = int(yesterday.timestamp()) * 1000
+
+        # Request to Spotify API
+        r = requests.get("https://api.spotify.com/v1/me/player/recently_played")
