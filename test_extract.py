dc507e33d6fc0370413cf21fe48cb39c6c0a6c63
Eric Dasse <christ.gououi@gmail.com>
2022-02-04 14:24:51 +0100

    [Fix] Add missing parentheses when using the extractor fixture

--- test_extract.py
+++ test_extract.py
@@ -6,8 +6,8 @@ import pytest
 
 class TestExtract:
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
-    extractor = script.Extract()
+    # yesterday = today - datetime.timedelta(days=1)
+    # extractor = script.Extract()
 
     @pytest.fixture
     def extractor(self):
@@ -18,7 +18,7 @@ class TestExtract:
 
     def test_data_validity(self, extractor):
         """Test the validity of the retrieved songs data"""
-        songs_df = extractor.retrieve_songs()
+        songs_df = extractor().retrieve_songs()
 
         # Is the result a Pandas DataFrame?
         assert isinstance(songs_df, pd.DataFrame)
@@ -31,7 +31,7 @@ class TestExtract:
 
     def test_retrieve_songs_timestamp(self, extractor):
         """Test the validity of the timestamp of songs retrieved within a given time period"""
-        songs_df = extractor.retrieve_songs(reference_date="30/11/2022", days=30)
+        songs_df = extractor().retrieve_songs(reference_date="30/11/2022", days=30)
         reference_date_object = datetime.datetime(2022, 1, 31)
         past_date = reference_date_object - datetime.timedelta(days=30)
         for timestamp in songs_df["timestamp"]:
