df1f2679847ff3944a1f1ddeb09afddcd311b033
Eric Dasse <christ.gououi@gmail.com>
2022-05-14 22:42:22 +0200

    Move `tests.py` from extraction module to tests module

--- spotify_feed/tests/tests.py
+++ spotify_feed/tests/tests.py
@@ -1,3 +1,5 @@
+import datetime
+
 from spotify_feed.extractor.tracks import get_recent_tracks, request_tracks_prior_to
 
 
@@ -16,16 +18,9 @@ def test_request_recently_played_tracks_before_a_date():
         assert datetime.datetime.strptime(played_at_day, "%Y-%m-%d") <= reference_date
 
 
-    assert data, "Unexpectedly get no songs before May 3rd, 2022"
-    assert (
-        "error" not in data.keys()
-    ), "There was an error while retrieving played tracks"
-    assert data["limit"] == 10
-    assert len(data["items"]) == 10
-
-
 def test_request_use_cache():
     _ = request_tracks_prior_to(before="2022-05-03", limit=10)
     response_2 = request_tracks_prior_to(before="2022-05-03", limit=10)
 
     assert response_2.from_cache
+
