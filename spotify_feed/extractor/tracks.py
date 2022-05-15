6ceea20040d824894906afc978de5e4da0684a24
Eric Dasse <christ.gououi@gmail.com>
2022-05-15 02:28:36 +0200

    tracks: add method to retrieve tracks played after a reference date

--- spotify_feed/extractor/tracks.py
+++ spotify_feed/extractor/tracks.py
@@ -13,8 +13,29 @@ requests_cache.install_cache(
 )
 
 
-def get_recent_tracks(before=None, limit=10):
-    response = request_tracks_prior_to(before, limit)
+def get_recent_tracks(before=None, after=None, limit=10):
+    """
+    Gets the most recently listened tracks of the user from Spotify API.
+
+    Parameters
+    ----------
+    before: str, optional
+        Date of reference before which the tracks were played. Formatted YYYY-mm-dd
+        (Default, None)
+        Ex. "2022-05-03"
+    after: str, optional
+        Date of reference after which the tracks were played. Formatted YYYY-mm-dd
+        (Default, None)
+    limit: int, optional
+        Maximum number of tracks to recover (default, 10)
+    """
+    if before and after:
+        raise AssertionError("You shouldn't define both before and after parameters")
+
+    if after:
+        response = request_tracks_after(after, limit=limit)
+    else:
+        response = request_tracks_prior_to(before, limit=limit)
 
     data = response.json()
 
@@ -37,6 +58,28 @@ def request_tracks_prior_to(before, limit):
 
     response = requests.get(
         url=f"{endpoint}?limit={limit}&before={unix_timestamp}",
+        headers=headers,
+    )
+
+    return response
+
+
+def request_tracks_after(after, limit):
+    token = os.environ.get("TOKEN")
+    endpoint = os.environ.get("ENDPOINT")
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",
+    }
+    if after:
+        date = datetime.datetime.strptime(after, "%Y-%m-%d")
+    else:
+        date = datetime.datetime.now()
+    unix_timestamp = int(date.timestamp()) * 1000
+
+    response = requests.get(
+        url=f"{endpoint}?limit={limit}&after={unix_timestamp}",
         headers=headers,
     )
 
--- spotify_feed/tests/tests.py
+++ spotify_feed/tests/tests.py
@@ -11,11 +11,28 @@ def test_request_recently_played_tracks_before_a_date():
         "error" not in data.keys()
     ), "There was an error while retrieving played tracks"
     assert data["limit"] == 10
+
     data_items = data["items"]
     assert len(data_items) == 10
     for data_item in data_items:
         played_at_day = data_item["played_at"][:10]
         assert datetime.datetime.strptime(played_at_day, "%Y-%m-%d") <= reference_date
+
+
+def test_request_recently_played_tracks_after_a_date():
+    data = get_recent_tracks(after="2022-05-03", limit=10)
+    reference_date = datetime.datetime.strptime("2022-05-03", "%Y-%m-%d")
+    assert data, "Unexpectedly get no songs before May 3rd, 2022"
+    assert (
+        "error" not in data.keys()
+    ), "There was an error while retrieving played tracks"
+    assert data["limit"] == 10
+
+    data_items = data["items"]
+    assert len(data_items) == 10
+    for data_item in data_items:
+        played_at_day = data_item["played_at"][:10]
+        assert datetime.datetime.strptime(played_at_day, "%Y-%m-%d") >= reference_date
 
 
 def test_request_use_cache():
