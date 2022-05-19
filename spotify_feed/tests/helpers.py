61ae00d7f2db2ad57b258d96f45fc4128febf6f5
Eric Dasse <christ.gououi@gmail.com>
2022-05-19 23:31:10 +0200

    Change reference date in helpers

--- spotify_feed/tests/helpers.py
+++ spotify_feed/tests/helpers.py
@@ -3,7 +3,7 @@ import datetime
 
 class Context:
     def __init__(self):
-        self.reference_day = "2022-05-03"
+        self.reference_day = "2022-05-15"
         self.reference_date = datetime.datetime.strptime(self.reference_day, "%Y-%m-%d")
         self.reference_date_str = datetime.datetime.strftime(
             self.reference_date, "%b %d, %Y"
